27b9fa76b8b2767f448eaba3612da3dd9f4fbbef
闫鑫 <332622154@qq.com>
2017-09-14 23:35:34 +0800

    no message

--- a.cpp
+++ a.cpp
@@ -5,6 +5,7 @@ int main()
 {
     printf("Hello Boost!\n");
     std::cout << "Hello World\n";
+    int a;
     return 0;
 }
 //g++ a.cpp  -I /usr/local/include -L /usr/local/lib -l boost_system -l boost_thread
