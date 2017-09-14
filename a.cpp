e6d94dc373104eeb94d93539e3ca3b3b3801cdc0
闫鑫 <332622154@qq.com>
2017-09-14 23:29:06 +0800

    修改1.0

--- a.cpp
+++ a.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <iostream>
 #include <boost/asio.hpp>
 int main()
 {
     printf("Hello Boost!\n");
-    
+    std::cout << "Hello World\n";
     return 0;
 }
 //g++ a.cpp  -I /usr/local/include -L /usr/local/lib -l boost_system -l boost_thread
